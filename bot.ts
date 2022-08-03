94af855aa199ef9e589b18ccac5e46179cea7243
0xDEAFBABE <deafbabe@startmail.com>
2022-08-04 03:06:50 +0600

    Done.

---
+++ bot.ts
@@ -0,0 +1,96 @@
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.10.1/mod.ts";
+import { InlineQueryResult } from "https://deno.land/x/grammy@v1.10.1/platform.deno.ts";
+
+const token = Deno.env.get("DECIDE_ROBOT_TOKEN");
+
+if (token === undefined || token === null) {
+  console.warn("Bot token is not set. Aborting...");
+  Deno.exit(1);
+}
+
+const yesOrNoOptions = ["Да", "Нет"];
+const rouletteOptions = [
+  "*Бах!*",
+  "*Клик*",
+  "*Клик*",
+  "*Клик*",
+  "*Клик*",
+  "*Клик*",
+];
+const elkOptions = [
+  "Да",
+  "Нет",
+  "Это не важно",
+  "Спок, бро",
+  "Толсто",
+  "Да, хотя зря",
+  "Никогда",
+  "100%",
+  "1 из 100",
+  "Еще разок",
+];
+
+const choice = <T>(variants: Array<T>): T =>
+  variants[Math.floor(Math.random() * variants.length)];
+const capitalizeFirstLetter = (str: string): string =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+const yesOrNo = (): string => choice(yesOrNoOptions);
+const roulette = (): string => choice(rouletteOptions);
+const elk = (): string => choice(elkOptions);
+const formatQuery = (query: string): string => {
+  query = query.trim();
+  while (query.endsWith("?")) query = query.substring(0, query.length - 1);
+  return capitalizeFirstLetter(query);
+};
+
+function generateInlineReply(
+  title: string,
+  question: string,
+  answer: string,
+): InlineQueryResult {
+  return {
+    type: "article",
+    id: crypto.randomUUID(),
+    title: title,
+    input_message_content: {
+      message_text: `${question}? <b>${answer}.</b>`,
+      parse_mode: "HTML",
+    },
+  };
+}
+
+const bot = new Bot(token);
+
+bot.inlineQuery(/.*( или .*)+/, async (ctx) => {
+  const query = formatQuery(ctx.inlineQuery.query);
+  const options = query.split(" или ").map(
+    capitalizeFirstLetter,
+  );
+
+  await ctx.answerInlineQuery([
+    generateInlineReply(
+      "Выбрать из вариантов.",
+      options.join(" или "),
+      choice(options),
+    ),
+  ], { cache_time: 0 });
+});
+
+bot.on("inline_query", async (ctx) => {
+  const query = formatQuery(ctx.inlineQuery.query);
+
+  if (query === "") {
+    await ctx.answerInlineQuery([], { cache_time: 30 * 24 * 3600 });
+  }
+
+  await ctx.answerInlineQuery([
+    generateInlineReply("Да или нет?", query, yesOrNo()),
+    generateInlineReply("Рулетка.", query, roulette()),
+    generateInlineReply("Сакральный олень.", query, elk()),
+  ], { cache_time: 0 });
+});
+
+await bot.init();
+console.log(`${bot.botInfo.username} is running...`);
+await bot.start();
